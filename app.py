1f57a62d87df0754d9572a6730fe03b5a22fb6ac
Alexander Ananiadis <agananiadis@gmail.com>
2016-05-02 19:24:00 -0400

    Testing Code

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 import settings
 import forecastio
 from pushbullet import Pushbullet
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim()
 
 pb = Pushbullet(settings.pushBulletApiKey)
 
 print(pb.devices)
+# push = pb.push_note("Today's Weather Update", "It's so cold. You should wear a jacket.")
+
+location = geolocator.geocode(settings.home)
+print((location.latitude, location.longitude))
+
+forecast = forecastio.load_forecast(settings.forecastioKey,location.latitude ,location.longitude )
+currCast = forecast.currently()
+print(currCast.summary)
+print(currCast.temperature)
+print(currCast.precipProbability)
+
+day = forecast.hourly()
+print(day.summary)
