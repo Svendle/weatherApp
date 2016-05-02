5229df902419fb1ef144a110d563b651452fc6ef
Alexander Ananiadis <agananiadis@gmail.com>
2016-05-02 19:48:56 -0400

    Rain predictions
    yay

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ print(pb.devices)
 # push = pb.push_note("Today's Weather Update", "It's so cold. You should wear a jacket.")
 
 location = geolocator.geocode(settings.home)
-print((location.latitude, location.longitude))
 
 forecast = forecastio.load_forecast(settings.forecastioKey,location.latitude ,location.longitude )
 currCast = forecast.currently()
@@ -20,4 +19,19 @@ print(currCast.temperature)
 print(currCast.precipProbability)
 
 day = forecast.hourly()
-print(day.summary)
+
+
+maxPrecipProb = 0
+for x in range(12):
+    print(day.data[x].precipProbability)
+    if day.data[x].precipProbability > maxPrecipProb:
+        maxPrecipProb = day.data[x].precipProbability
+
+if maxPrecipProb > .85:
+    pb.push_note("Today's Weather Update", "It is going to rain")
+elif maxPrecipProb > .50:
+    pb.push_note("Today's Weather Update", "It's probably going to rain")
+elif maxPrecipProb > .20:
+    pb.push_note("Today's Weather Update", "It's probably not going to rain")
+else:
+    pb.push_note("Today's Weather Update", "It's not going to rain")
