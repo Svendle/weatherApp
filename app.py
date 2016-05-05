f953ce9ff9361704e5f3a3051a665f753c4ab105
Shiv Krishnan <rskrishnan@rs-macbook-pro.mtw.jhu.edu>
2016-05-05 11:28:01 -0400

    Changes

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import settings
 import forecastio
+import sys
 from pushbullet import Pushbullet
 from geopy.geocoders import Nominatim
+
 
 geolocator = Nominatim()
 
@@ -13,19 +15,34 @@ print(pb.devices)
 location = geolocator.geocode(settings.home)
 
 forecast = forecastio.load_forecast(settings.forecastioKey,location.latitude ,location.longitude )
-currCast = forecast.currently()
-print(currCast.summary)
-print(currCast.temperature)
-print(currCast.precipProbability)
 
-day = forecast.hourly()
-
+print("Choose for when you want to see the weather (Please select a number):")
+print("1.Right Now")
+print("2.Now to n hours from now(max n = 48)")
+print("3.From now to n days onward(max n = 7)")
+choice = input("Choice: ")
+day = forecast.currently()
+if choice == '1':
+    day = forecast.currently()
+    print(day.summary)
+    print(day.temperature)
+    print(day.precipProbability)
+    sys.exit()
+elif choice == '2':
+    day = forecast.hourly()
+    print(day.summary);
+elif choice == '3':
+    day = forecast.daily();
+    print(day.summary);
+else:
+    print("Incorrect choice");
+    sys.exit();
 
 maxPrecipProb = 0
-for x in range(12):
-    print(day.data[x].precipProbability)
-    if day.data[x].precipProbability > maxPrecipProb:
-        maxPrecipProb = day.data[x].precipProbability
+for x in day.data:
+    print(x.precipProbability)
+    if x.precipProbability > maxPrecipProb:
+        maxPrecipProb = x.precipProbability
 
 if maxPrecipProb > .85:
     pb.push_note("Today's Weather Update", "It is going to rain")
